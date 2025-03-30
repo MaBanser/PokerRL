6c7cd414b47806bed55e41267a310674c3441ef9
MaBanser <mbanser@uos.de>
2025-03-30 22:19:33 +0200

    Initial setup

---
+++ Check_installation.bat
@@ -0,0 +1,6 @@
+conda activate PokerRL
+python
+import torch
+print(torch.rand(5,3)
+torch.cuda.is_available()
+pause
---
+++ Install_environment.bat
@@ -0,0 +1,3 @@
+conda env create -f PokerRL.yml
+conda activate PokerRL
+pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu126
